4dc9d05d8443767f27c0a5d5c03110012129893b
ehsan16 <ehsan_haidari_11@hotmail.com>
2019-03-13 10:13:51 +0200

    First commit

--- src/main/java/hh/swd20/Bookstore/domain/Book.java
+++ src/main/java/hh/swd20/Bookstore/domain/Book.java
@@ -1,13 +1,59 @@
 package hh.swd20.Bookstore.domain;
 
-import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
 
 public class Book {
-private String title;
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
 private String author;
-private int year;
+private String title;
+private String year;
 private String isbn;
-private BigDecimal price;
+
+@ManyToOne
+@JsonIgnore
+@JoinColumn(name = "categoryid")
+private Category category;
+
+public Book() {}
+
+public Book(String author, String title, String year, String isbn, Category category) {
+	super();
+	this.author=author;
+	this.title=title;
+	this.year=year;
+	this.isbn=isbn;
+	this.category=category;
+}
+public Category getCategory() {
+	return category;
+}
+
+public void setCategory(Category category) {
+	this.category = category;
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
 public String getTitle() {
 	return title;
 }
@@ -20,22 +66,28 @@ public String getAuthor() {
 public void setAuthor(String author) {
 	this.author = author;
 }
-public int getYear() {
+public String getYear() {
 	return year;
 }
-public void setYear(int year) {
+public void setYear(String year) {
 	this.year = year;
 }
-public BigDecimal getPrice() {
-	return price;
-}
-public void setPrice(BigDecimal price) {
-	this.price = price;
-}
+
 public String getIsbn() {
 	return isbn;
 }
 public void setIsbn(String isbn) {
 	this.isbn = isbn;
 }
+
+@Override
+public String toString() {
+	if (this.category != null)
+	return "Book [id=" + id + ", author=" + author + ", title=" + title + ", year=" + year + ", isbn=" + isbn
+			+ ", category=" + category + "]";
+	else 
+		return "Book [id=" + id + ", author=" + author + ", title=" + title + ",year=" + year + ", isbn=" + isbn;
+}
+
+
 }
